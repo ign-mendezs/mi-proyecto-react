c9e06e7527cfa9a328357ce31022ca1660287b24
SansReves <ig.mendezs@duocuc.cl>
2025-03-05 15:32:10 -0300

    edit products css

--- src/pages/productos.js
+++ src/pages/productos.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 function Productos() {
   return (
-    <div className="p-4">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
       <h1 className="text-2xl font-bold mb-6">Página de Productos</h1>
+
+      <img src="/turner.png" alt="turner" className="mb-6"/>
 
       <div className="mb-6">
         <h2 className="text-lg font-semibold mb-2">Aquí pondría productos, si tuviera alguno.</h2>
-        <p></p>
         <p>Actualmente, no hay productos disponibles.</p>
       </div>
     </div>
