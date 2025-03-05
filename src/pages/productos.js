45c2f9c417d8ef52dcdef83aef8895feb12bac93
SansReves <ig.mendezs@duocuc.cl>
2025-03-05 17:17:10 -0300

    .

--- src/pages/productos.js
+++ src/pages/productos.js
@@ -5,11 +5,14 @@ function Productos() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
       <h1 className="text-2xl font-bold mb-6">Página de Productos</h1>
 
+      <div className="mb-6">
+        <h2 className="text-lg font-semibold mb-2">Aquí pondría productos</h2>
+      </div>
+
       <img src="/turner.png" alt="turner" className="mb-6"/>
 
       <div className="mb-6">
-        <h2 className="text-lg font-semibold mb-2">Aquí pondría productos, si tuviera alguno.</h2>
-        <p>Actualmente, no hay productos disponibles.</p>
+        <h2 className="text-lg font-semibold mb-2">Si tuviera alguno</h2>
       </div>
     </div>
   );
