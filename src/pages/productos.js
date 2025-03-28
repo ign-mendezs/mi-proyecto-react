0acc8446519f3762c697ea3c4f75fa5884e24d47
SansReves <ig.mendezs@duocuc.cl>
2025-03-28 11:36:26 -0300

    feat: add ProtectedRoute y update pagina producto

--- src/pages/productos.js
+++ src/pages/productos.js
@@ -1,24 +1,20 @@
+import React, { useEffect, useContext } from 'react';
 import { useRouter } from 'next/router';
-import React, { useState, useEffect } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 function Productos() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const router = useRouter();
+  const router = useRouter();
+  const { isAuthenticated } = useContext(AuthContext);
 
-    useEffect(() => {
-        // Leer la cookie manualmente
-        const cookies = document.cookie.split('; ').find(row => row.startsWith('sessionToken='));
-
-        if (!cookies) {
-            router.push('/error'); // Redirigir si no hay sesión
-        } else {
-            setIsAuthenticated(true);
-        }
-    }, []);
-
+  useEffect(() => {
     if (!isAuthenticated) {
-        return null; // Evita que la página se renderice antes de redirigir
+      router.push('/error'); // o redirigir a '/login'
     }
+  }, [isAuthenticated, router]);
+
+  if (!isAuthenticated) {
+    return null; // Evita renderizar la página mientras se redirige
+  }
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
@@ -28,7 +24,7 @@ function Productos() {
         <h2 className="text-lg font-semibold mb-2">Aquí pondría productos</h2>
       </div>
 
-      <img src="/turner.png" alt="turner" className="mb-6"/>
+      <img src="/turner.png" alt="turner" className="mb-6" />
 
       <div className="mb-6">
         <h2 className="text-lg font-semibold mb-2">Si tuviera alguno</h2>
