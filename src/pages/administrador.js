c1340071a89e3122b2ff1974c9a464ac23bdefc3
SansReves <ig.mendezs@duocuc.cl>
2025-04-01 20:39:59 -0300

    feat: add pages to navbar

---
+++ src/pages/administrador.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useContext } from 'react';
+import withAuthProtection from '../components/withAuthProtection';
+
+function Administrador() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
+      <h1 className="text-2xl font-bold mb-6">Página de Administracion</h1>
+
+    </div>
+  );
+}
+
+export default withAuthProtection(Administrador, ['admin']);
