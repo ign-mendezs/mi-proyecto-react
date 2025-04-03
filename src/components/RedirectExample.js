1a6f4c4f354a4f2dc47b6c21ff61bd2309968864
SansReves <ig.mendezs@duocuc.cl>
2025-04-03 12:57:53 -0300

    change redirect to a component

---
+++ src/components/RedirectExample.js
@@ -0,0 +1,42 @@
+// src/components/RedirectExample.js
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function RedirectExample() {
+  const router = useRouter();
+  const [countdown, setCountdown] = useState(5);
+  const [isStarted, setIsStarted] = useState(false);
+
+  useEffect(() => {
+    if (!isStarted) return; // Solo inicia si isStarted es true
+
+    const interval = setInterval(() => {
+      setCountdown((prevCountdown) => {
+        if (prevCountdown <= 1) {
+          clearInterval(interval);
+          router.replace('/'); // Redirige cuando llegue a 0
+          return 0;
+        }
+        return prevCountdown - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [isStarted, router]);
+
+  return (
+    <div className="card flex flex-col items-center justify-center">
+      <h2 className="font-bold mb-2">Redirección con Cuenta Regresiva</h2>
+      {isStarted ? (
+        <p>Serás redirigido en {countdown} segundos...</p>
+      ) : (
+        <button 
+          onClick={() => setIsStarted(true)} 
+          className="btn btn-blue"
+        >
+          Iniciar Redirección
+        </button>
+      )}
+    </div>
+  );
+}
