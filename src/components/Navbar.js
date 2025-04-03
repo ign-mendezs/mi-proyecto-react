57aedf2d16b7f5cab09d1da3b8d9686b57cd3f0a
SansReves <ig.mendezs@duocuc.cl>
2025-04-03 12:41:48 -0300

    change navbar options

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,28 @@
 // src/components/Navbar.js
 import Link from 'next/link';
-import React from 'react';
 import LogoutButton from './LogoutButton';
+import { AuthContext } from '../context/AuthContext';
+import React, { useState, useContext } from 'react';
 
 export default function Navbar() {
+  const { isAuthenticated } = useContext(AuthContext);
+
   return (
     <nav className="bg-primary text-textSecondary p-4">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
-        <h1 className="text-xl font-bold">Mi Proyecto</h1>
+        <h1 className="text-xl font-bold">Mi MVP React</h1>
 
         {/* Links */}
-        <ul className="flex space-x-6">
+        <ul className="flex items-center space-x-6">
           <li>
             <Link href="/" className="hover:bg-green-800 px-4 py-2 rounded transition">
               👤 Inicio
+            </Link>
+          </li>
+          <li>
+            <Link href="/fundamentos-react" className="hover:bg-green-800 px-4 py-2 rounded transition">
+              📚 Fundamentos de React
             </Link>
           </li>
           <li>
@@ -23,23 +31,23 @@ export default function Navbar() {
             </Link>
           </li>
           <li>
-            <Link href="/redirigir" className="hover:bg-green-800 px-4 py-2 rounded transition">
-              🔄 Prueba para redirigir
-            </Link>
-          </li>
-          <li>
             <Link href="/administrador" className="hover:bg-green-800 px-4 py-2 rounded transition">
               🛠️ Administracion
             </Link>
           </li>
-          <li>
-            <Link href="/login" className="hover:bg-green-800 px-4 py-2 rounded transition">
-              Login
-            </Link>
-          </li>
-          <li>
-            <LogoutButton />
-          </li>
+          {!isAuthenticated && (
+            <li>
+              <Link href="/login" className="hover:bg-green-800 px-4 py-2 rounded transition">
+                Login
+              </Link>
+            </li>
+          )}
+
+          {isAuthenticated && (
+            <li>
+              <LogoutButton />
+            </li>
+          )}
         </ul>
       </div>
     </nav>
